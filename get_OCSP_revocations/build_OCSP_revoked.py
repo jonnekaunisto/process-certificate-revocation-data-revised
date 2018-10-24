bb8b93d3c7c256c7010659e04f665bc0e1220f85
hmyan90 <34290458+hmyan90@users.noreply.github.com>
2018-10-24 13:06:43 -0700

    Update build_OCSP_revoked.py

--- get_OCSP_revocations/build_OCSP_revoked.py
+++ get_OCSP_revocations/build_OCSP_revoked.py
@@ -1,13 +1,14 @@
 from urlparse import urlparse
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Value
 import httplib, sys, urllib
 import json
 from oscrypto import asymmetric
 from ocspbuilder import OCSPRequestBuilder
 from asn1crypto import core, ocsp
 import base64
+import time
 
-workers = 256
+workers = 16
 outfile = './OCSP_revoked/certs'
 infile = '../certs_without_crl.json'
 
@@ -22,12 +23,14 @@ issuerCerts['http://ocsp.int-x2.letsencrypt.org'] = asymmetric.load_certificate(
 issuerCerts['http://ocsp.int-x3.letsencrypt.org'] = asymmetric.load_certificate(issuer3)
 issuerCerts['http://ocsp.int-x4.letsencrypt.org'] = asymmetric.load_certificate(issuer4)
 
-def doWork(i):
+def doWork(i, q, check_finish):
     print('starting worker ' + str(i))
     out = open(outfile + str(i) + '.json', 'w')
     while True:
         try:
-            cert = json.loads(q.get())
+            if check_finish.value and q.empty():
+                break
+            cert = json.loads(q.get(timeout=1))
             if(cert['parsed']['extensions']['authority_info_access']['ocsp_urls']):
                 urlList = cert['parsed']['extensions']['authority_info_access']['ocsp_urls']
                 url = urlList[0].rstrip('/').lstrip(' ')
@@ -36,6 +39,7 @@ def doWork(i):
                         out.write(json.dumps(cert) + '\n')
         except:
             pass
+    print("worker %d exit" %i)
 
 def isRevoked(url, rawCert):
     subject_cert = asymmetric.load_certificate(base64.b64decode(rawCert))
@@ -51,15 +55,18 @@ def isRevoked(url, rawCert):
 
 if __name__ == '__main__':
     q = Queue(workers * 16)
+    check_finish = Value('i', 0)
     for i in range(workers):
-        p = Process(target=doWork, args=(i,))
+        p = Process(target=doWork, args=(i, q, check_finish))
         p.start()
     try:
         ctr = 0
         for cert in open(infile, 'r'):
             q.put(cert)
             ctr += 1
-            if(ctr % 1000 == 0):
+            if(ctr % 10000 == 0):
                 print(str(ctr) + " certificates processed")
+        check_finish.value = 1
+        print("End of put certificates into queue")
     except KeyboardInterrupt:
         sys.exit(1)
