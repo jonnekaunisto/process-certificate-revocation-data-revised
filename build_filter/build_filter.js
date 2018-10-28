f78bb02dd9ffe93b0a0a625060055aa0bb6fa7b4
hmyan90 <34290458+hmyan90@users.noreply.github.com>
2018-10-28 16:20:34 -0700

    Update build_filter.js

--- build_filter/build_filter.js
+++ build_filter/build_filter.js
@@ -1,28 +1,57 @@
+const assert = require('assert');
+
 const MLBFilter = require("multi-level-bloom-filter-js");
 const LineByLine = require("n-readlines");
 
-const REVOKED = 800000;
-const UNREVOKED = 62000000;
+const REVOKED = 524;
+const UNREVOKED = 342016;
 const FP_RATE = 0.5;
 const FP1_RATE = REVOKED * Math.sqrt(FP_RATE) / UNREVOKED;
 const REVOKED_FILENAME = '../final_revoked.json';
 const UNREVOKED_FILENAME = '../final_unrevoked.json';
 
-
+var cnt = 0;
 let revoked  = []
 let liner = new LineByLine(REVOKED_FILENAME);
 let line;
 while (line = liner.next()) {
+  cnt += 1;
   line = line.toString();
   revoked.push(line);
+  if (cnt == REVOKED) {
+    break;
+  }
 }
 
-let unrevoked = ['test']
+var cnt = 0;
+let unrevoked = []
 liner = new LineByLine(UNREVOKED_FILENAME);
 while (line = liner.next()) {
+  cnt += 1;
   line = line.toString();
   unrevoked.push(line);
+  if (cnt == UNREVOKED) {
+    break;
+  }
 }
 
 let mlbf = new MLBFilter(REVOKED, UNREVOKED, revoked, unrevoked, FP_RATE, FP1_RATE);
-console.log(mlbf.toJSON());
+// console.log(mlbf.toJSON());
+
+error = 0
+revoked.forEach(function(x){
+  // contains: false means in S, true means in R
+  // assert(mlbf.contains(x));
+  if (mlbf.contains(x) == false) {
+    error += 1;
+  }
+});
+
+unrevoked.forEach(function(x){
+  // assert(!mlbf.contains(x));
+  if (mlbf.contains(x) == true) {
+    error += 1;
+  }
+});
+
+console.log(error)
